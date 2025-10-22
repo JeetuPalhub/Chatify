ac1a8ad474e4abb33099471307055fbb556b5ebc
JeetuPalhub <jeetupal.pal31@gmail.com>
2025-10-22 22:27:37 +0530

    token generated

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,8 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs"
+import { generateToken } from "../lib/utils.js";
+
+
 export const signup = async(req, res) => {
 const {fullName, email, password} = req.body
 
@@ -16,9 +21,35 @@ try {
       return res.status(400).json({ message: "Invalid email format" });
     }
     
+const user = await User.findOne({email});
+if(user) return res.status(400).json({message:"Email already exists"})
 
+// 123456 => &weferhrqree_refeggg
+
+const salt = await bcrypt.genSalt(10)
+const hashedPassword = await bcrypt.hash(password,salt)
+
+const newUser = new User({
+    fullName,
+    email,
+    password: hashedPassword
+})
+
+if(newUser) {
+    generateToken(newUser._id, res)
+    await newUser.save()
+
+    res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+    });
+}else{
+    res.status(400).json({message: "Invalid user data"});
+}
 
 } catch (error) {
-    
+    console.log()
 }
 }
